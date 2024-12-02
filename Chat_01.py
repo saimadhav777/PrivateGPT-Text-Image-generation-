149b4f98b56fb17aab4e0de0350bbd26f6d14f93
sai madhav <gsaimadhav623@gmail.com>
2024-12-02 11:07:28 +0530

    Add files via upload

---
+++ Chat_01.py
@@ -0,0 +1,84 @@
+import ollama
+import streamlit as st
+from openai import OpenAI
+
+##UI configuration.
+st.set_page_config(
+    page_title="Chat Playground",
+    page_icon="💬",
+    layout="wide",
+    initial_sidebar_state="expanded",
+)
+
+
+def extract_model_names(model_info:list) -> tuple:
+    """
+    Extracts the model names from the models information.
+    :param models_info: A directory containing the models' information.
+    return:
+        This function returns a tuple containing the model names.
+    """
+    return tuple(model["name"] for model in model_info["models"])
+
+def main():
+    """
+    The main function that runs the application.
+    """
+    # page_icon("💬")
+    st.subheader("Ollama Playground",divider="red",anchor=False)
+    
+    client = OpenAI(
+        base_url="http://localhost:11434/v1",
+        api_key="ollama",
+    )
+    
+    models_info = ollama.list()
+    available_models = extract_model_names(models_info)
+    
+    if available_models:
+        selected_model = st.selectbox(
+            "Pick a model available locally on your system 👇", available_models
+        )
+    else:
+        st.warning("You have not pulled any model from Ollama yet!",icon="⚠️")
+        if st.button("Go to settings to download a model"):
+            st.switch_page("pages/setting_03.py")    
+            
+    message_container = st.container(height=500, border=True)
+    
+    if "messages" not in st.session_state:
+        st.session_state.messages = []
+    
+    for message in st.session_state.messages:
+        avatar = "🤖" if message["role"] == "assistant" else "😎"
+        with message_container.chat_message(message["role"],avatar=avatar):
+            st.markdown(message["content"])
+            
+    if prompt := st.chat_input("Enter a prompt here..."):
+        try:
+            st.session_state.messages.append(
+                {"role": "user", "content": prompt}
+            )
+            
+            message_container.chat_message("user",avatar="😎").markdown(prompt)
+            
+            with message_container.chat_message("assistant",avatar="🤖"):
+                with st.spinner("model working...."):
+                    stream = client.chat.completions.create(
+                        model = selected_model,
+                        messages=[
+                            {"role":m["role"],"content": m["content"]}
+                            for m in st.session_state.messages
+                        ],
+                        stream=True,
+                    )
+                #stream response
+                response = st.write_stream(stream)
+            st.session_state.messages.append(
+                {"role": "assistant", "content": response})
+        except Exception as e:
+            st.error(e,icon="⛔")             
+                       
+if __name__ == "__main__":
+    main()                       
+             
---
+++ setting_03.py
@@ -0,0 +1,94 @@
+import streamlit as st
+import ollama
+from time import sleep
+
+st.set_page_config(
+    page_title="Model Management",
+    page_icon="🔩",
+    layout="wide",
+    initial_sidebar_state="expanded",
+)
+
+def main():
+    st.subheader("Model Management",divider="red",anchor=False)
+    
+    st.subheader("Download Models",anchor=False)
+    model_name = st.text_input(
+        "Enter the name of the model to download 👇",placeholder="mistral"
+    )
+    
+    if st.button(f"📥 :green[**Download**] :red[{model_name}]"):
+        if model_name:
+            try:
+                ollama.pull(model_name)
+                st.success(f"Downloaded Model: {model_name}",icon="🎉")
+                st.balloons()
+                sleep(1)
+                st.rerun()
+            except Exception as e:
+                st.error(f"""Failed to download model: {model_name}. Error: {str(e)}""",icon="😒",
+                         )
+        else:
+            st.warning("Please enter a model name.", icon="⚠️")
+            
+    st.divider()
+    
+    st.subheader("Create model",anchor=False)
+    modelfile = st.text_area(
+        "Enter the modelfile",
+        height=100,
+        placeholder="""From mistral 
+        SYSTEM You are mario from super mario bros.""",
+    )
+    model_name = st.text_input(
+        "Enter the name of the model to create", placeholder="mario"
+    )
+    if st.button(f"🆕 Create Model {model_name}"):
+        if model_name and modelfile:
+            try:
+                ollama.create(model=model_name, modelfile=modelfile)
+                st.success(f"Created model: {model_name}", icon="✅") 
+                st.balloons()
+            except Exception as e:
+                st.error(
+                    f"""Failed to create model:{model_name}. Error: {str(e)}""",
+                    icon="😒",
+          
+                )
+        else:
+              st.warning("Please enter a **model name** and **modelfile**",icon="⚠️") 
+    st.divider()
+    
+    st.subheader("Delete Models",anchor=False)
+    models_info = ollama.list()
+    
+    available_models = [m["name"] for m in models_info["models"]]
+    
+    if available_models:
+        selected_models = st.multiselect(
+            "Select models to delete 👇", available_models
+        )
+        
+        if st.button("🗑️ :red[**Delete Selected Model(s)**]"):
+            for model in selected_models:
+               try:
+                  ollama.delete(model)
+                  st.success(f"Deleted model: {model}", icon="🎉")
+                  st.balloons()
+                  sleep(1)
+                  st.rerun()
+               except Exception as e:
+                   st.error(
+                       f"""Failed to delete model: {model}. Error: {str(e)}""",
+                       icon="😒",
+                   )   
+    else:
+        st.info("No models available for deletion.",icon="🦗")
+        
+
+if __name__ == "__main__":
+    main()
+
+            
+                                                  
+            
